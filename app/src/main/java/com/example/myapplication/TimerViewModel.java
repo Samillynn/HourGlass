43c0c1d504eafeb718167d08234c05b407662573
LHJJosh <91180701+LHJJosh@users.noreply.github.com>
2022-04-13 22:06:01 +0800

    Update TimerViewModel.java

--- app/src/main/java/com/example/myapplication/TimerViewModel.java
+++ app/src/main/java/com/example/myapplication/TimerViewModel.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class TimerViewModel extends ViewModel {
+    //Mutable variables so that changes to variables can be observed
     private final MutableLiveData<Integer> totalTimeSecs = new MutableLiveData<>();
     private final MutableLiveData<Integer> leftTimeSecs = new MutableLiveData<>();
 

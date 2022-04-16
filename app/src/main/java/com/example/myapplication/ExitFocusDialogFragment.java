b6e0af15bfe650aa66acac065871949e6becfeaa
LHJJosh <91180701+LHJJosh@users.noreply.github.com>
2022-04-16 19:48:00 +0800

    Update ExitFocusDialogFragment.java
    Added random question generation

--- app/src/main/java/com/example/myapplication/ExitFocusDialogFragment.java
+++ app/src/main/java/com/example/myapplication/ExitFocusDialogFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -12,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import java.util.HashMap;
+import java.util.Random;
 import java.util.function.Consumer;
 
 public class ExitFocusDialogFragment extends DialogFragment {
@@ -26,6 +29,19 @@ public class ExitFocusDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         final EditText input = new EditText(getContext());
+        //Setting up the questions Hashmap
+        Questions questions = new Questions();
+        questions.Questions();
+        
+        //Getting hashmap
+        HashMap exitQuestions = questions.getMap();
+        
+        //Getting random number
+        Random rand = new Random();
+        int size = exitQuestions.size();
+        int random = rand.nextInt(size);
+
+        String[] questionsAnswers = (String[]) exitQuestions.get(random);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT);
@@ -33,10 +49,10 @@ public class ExitFocusDialogFragment extends DialogFragment {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext())
                 .setView(input)
                 .setTitle("Emergency Exit")
-                .setMessage("Please answer to exit\nWhat is 3x3?")
+                .setMessage("Please answer to exit\n" + questionsAnswers[0])
                 .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        if(input.getText().toString().equals("9")){
+                        if(input.getText().toString().equals(questionsAnswers[1])){
                             activityCallback.accept(true);
                             dialog.dismiss();
                         } else {
